2d93303c42bd3699a6d6bf93390ba06bdf12ff42
christian zambrana <chrislajas@gmail.com>
2019-05-24 03:47:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log(); string.toLowerCase()
-}
 
-function logWhisper(string) {
-    console.log(); string.toUpperCase()
-}
+
 
 
   
